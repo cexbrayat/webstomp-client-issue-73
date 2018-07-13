d066308029e57a3ac1d0993c3e165525724bcc4b
cexbrayat <cedric@ninja-squad.com>
2018-07-13 12:48:53 +0200

    feat: add webstomp-client example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Webstomp from 'webstomp-client';
+import { Client} from 'webstomp-client';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Webstomp';
+
+  constructor() {
+    const url = 'ws://ponyracer.ninja-squad.com/ws';
+    const websocket: WebSocket = new WebSocket(url);
+    const stompClient: Client = Webstomp.over(websocket);
+    stompClient.connect({ login: null, passcode: null }, () => {
+        stompClient.subscribe('player/2179', message => {
+        const bodyAsJson = JSON.parse(message.body);
+        this.title = bodyAsJson;
+      });
+    });
+  }
 }
